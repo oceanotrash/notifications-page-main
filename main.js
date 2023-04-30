3a5b3f487c141365b01d9f73f7727031ca4b114a
oceanotrash <estivaericjan@gmail.com>
2023-04-30 18:53:39 +0800

    added scroll bar

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
-let button = document.getElementById("read");
+// let button = document.getElementById("read");
 
+// button.addEventListener("click", () => {
+//   document.querySelectorAll(".single-box").forEach((e) => {
+//     e.classList.remove("unseen");
+//   });
+
+//   document.querySelectorAll(".dot").forEach((e) => {
+//     e.classList.remove("dot");
+//   });
+
+//   document.getElementById('num').innerText = "0";
+// });
+
+// Get the button element with the id "read"
+const button = document.getElementById("read");
+
+// Attach a click event listener to the button
 button.addEventListener("click", () => {
-  document.querySelectorAll(".single-box").forEach((e) => {
-    e.classList.remove("unseen");
+  // Get all elements with the class "single-box"
+  const singleBoxes = document.querySelectorAll(".single-box");
+
+  // Loop through each single-box element and remove the class "unseen"
+  singleBoxes.forEach((box) => {
+    box.classList.remove("unseen");
   });
 
-  document.querySelectorAll(".dot").forEach((e) => {
-    e.classList.remove("dot");
+  // Get all elements with the class "dot"
+  const dots = document.querySelectorAll(".dot");
+
+  // Loop through each dot element and remove the class "dot"
+  dots.forEach((dot) => {
+    dot.classList.remove("dot");
   });
 
-
-  document.getElementById('num').innerText = "0";
+  // Update the text of the element with the id "num" to "0"
+  document.getElementById("num").innerText = "0";
 });
